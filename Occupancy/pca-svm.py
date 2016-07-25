31737f15e8dd0d398454c560f72027e08e2d2c18
bontorhumala <bontorhumala@gmail.com>
2016-07-25 23:39:35 +0700

    remove negative data

--- Occupancy/pca-svm.py
+++ Occupancy/pca-svm.py
@@ -5,19 +5,20 @@ from sklearn.metrics import accuracy_score
 from sklearn import svm
 import os
 import time
+import matplotlib.pyplot as plt
 
 # Constants
 START_IDX = 21600;
 END_IDX = 80100;
-DELTA = 25.0; # >30 watts indicates on off events
+DELTA = 50.0; # >30 watts indicates on off events
 
 # load data from CSV files inside the directory
 def load_data(dir_path):
 	a_data = []; # alltime data; d_da ta is daily data
 	for i in os.listdir(dir_path):
 		if i.endswith(".csv"):
-			# read the data from 6 AM to 10 PM (data 21600 to 82800)
-			d_data = pd.read_csv(filepath_or_buffer = dir_path + i, header=None, sep=',');
+			# read the data from 6 AM to 10 PM (data 21600 to 80100)
+			d_data = pd.read_csv(filepath_or_buffer = dir_path + i, header=None, sep=',', usecols=[0,1,2]);
 			a_data.append(d_data[START_IDX:END_IDX:1]);
 	return a_data;	
 	
@@ -41,25 +42,25 @@ def calculate_onoff(slot_data):
 def compute_feature(slot, slot_data):
     power123 = slot_data.ix[:,[0,1,2]].sum(axis=1);
 
-    min1 = slot_data.ix[:,[0]].min(); 
-    min2 = slot_data.ix[:,[1]].min();
-    min3 = slot_data.ix[:,[2]].min();
+    min1 = slot_data.ix[:,[0]].min().values[0]; 
+    min2 = slot_data.ix[:,[1]].min().values[0];
+    min3 = slot_data.ix[:,[2]].min().values[0];
     min123 = power123.min();
-    max1 = slot_data.ix[:,[0]].max();
-    max2 = slot_data.ix[:,[1]].max();
-    max3 = slot_data.ix[:,[2]].max();
+    max1 = slot_data.ix[:,[0]].max().values[0];
+    max2 = slot_data.ix[:,[1]].max().values[0];
+    max3 = slot_data.ix[:,[2]].max().values[0];
     max123 = power123.max();
-    mean1 = slot_data.ix[:,[0]].mean();
-    mean2 = slot_data.ix[:,[1]].mean();
-    mean3 = slot_data.ix[:,[2]].mean();
+    mean1 = slot_data.ix[:,[0]].mean().values[0];
+    mean2 = slot_data.ix[:,[1]].mean().values[0];
+    mean3 = slot_data.ix[:,[2]].mean().values[0];
     mean123 = power123.mean();
-    std1 = slot_data.ix[:,[0]].std();
-    std2 = slot_data.ix[:,[1]].std();
-    std3 = slot_data.ix[:,[2]].std();
+    std1 = slot_data.ix[:,[0]].std().values[0];
+    std2 = slot_data.ix[:,[1]].std().values[0];
+    std3 = slot_data.ix[:,[2]].std().values[0];
     std123 = power123.std();
-    sad1 = calculate_sad(slot_data.iloc[:,[0]]);
-    sad2 = calculate_sad(slot_data.iloc[:,[1]]);
-    sad3 = calculate_sad(slot_data.iloc[:,[2]]);
+    sad1 = calculate_sad(slot_data.iloc[:,[0]]).values[0];
+    sad2 = calculate_sad(slot_data.iloc[:,[1]]).values[0];
+    sad3 = calculate_sad(slot_data.iloc[:,[2]]).values[0];
     sad123 = calculate_sad(slot_data.iloc[:,[0,1,2]].sum(axis=1).to_frame());
     corl1 = slot_data.ix[:,0].autocorr();
     corl2 = slot_data.ix[:,1].autocorr();
@@ -90,13 +91,13 @@ def compute_pprob(occ_data):
 		occ_prob = pd.concat([occ_prob, occ_sum], axis=1);	
 	occ_prob = occ_prob.div(900);
 	occ_prob = occ_prob.stack();
-	occ_prob.columns = ['pprob'];
 	return occ_prob;
 
 # extract features from raw_data
 def extract_features(raw_data, occ_data):
 	a_features = [];
 	for day in raw_data:
+		day = day[day > 0].dropna();
 		d_features = pd.DataFrame(columns=('min1', 'min2', 'min3', 'min123', 'max1', 'max2', 'max3', 'max123', 'mean1', 'mean2', 'mean3', 'mean123', 'std1', 'std2', 'std3', 'std123', 'sad1', 'sad2', 'sad3', 'sad123', 'corl1', 'corl2', 'corl3', 'corl123', 'onoff1', 'onoff2', 'onoff3', 'onoff123', 'range1', 'range2', 'range3', 'range123', 'pfixed', 'ptime'));
 		# iterate over 16 * 4 + 1` slots = 65 slots (16 from 6AM to 9PM, 4 from 15 mins interval, 10PM only contributes 1)
 		for slot in range(0, 65):
@@ -111,6 +112,7 @@ def extract_features(raw_data, occ_data):
 	pprob = pprob.reset_index();
 	dropped_cols = [0,1];
 	pprob = pprob.drop(pprob.columns[dropped_cols], 1);
+	pprob.columns = ['pprob'];
 	total_features = total_features.join(pprob);
 	return total_features;
 
@@ -147,35 +149,39 @@ print("--- load occ_training_label: %s seconds ---" % (time.time() - start_time)
 
 start_time = time.time();
 all_features = extract_features(a_data, occ_training_data);
+f = open('all_features.csv', 'w');
+f.write(all_features.to_csv());
+f.close();
 print("--- extract all_features: %s seconds ---" % (time.time() - start_time));
 
 # load the features into pca	
 start_time = time.time();
 pca = PCA();
 pca.fit(all_features);
-num_comp = 0;
-comp_sum = 0.0;
-ev_sum = np.sum(pca.explained_variance_);
 print("--- load training data to PCA: %s seconds ---" % (time.time() - start_time));
 
 # take only L components that make up the 95% variance
 start_time = time.time();
-for ev in pca.explained_variance_:
+num_comp = 0;
+comp_sum = 0.0;
+ev_sum = np.sum(pca.explained_variance_ratio_);
+for ev in pca.explained_variance_ratio_:
 	num_comp = num_comp+1;
 	comp_sum = comp_sum+ev;
 	if ((comp_sum / ev_sum) > 0.95):
 		break;
+print("--- pca.n_components: %s ---" % num_comp);
 print("--- find 0.95 variance: %s seconds ---" % (time.time() - start_time));
 
+# run PCA
 start_time = time.time();
 pca.n_components = num_comp;
 all_features_reduced = pca.fit_transform(all_features);
+print all_features_reduced.shape;
 print("--- run PCA for training: %s seconds ---" % (time.time() - start_time));
 
-# load to SVM
+# run SVM classifier
 svc = svm.SVC(kernel='linear');
-
-# load occupancy ground truth
 start_time = time.time();
 svc.fit(all_features_reduced, occ_training_label);
 print("--- run SVC: %s seconds ---" % (time.time() - start_time));
