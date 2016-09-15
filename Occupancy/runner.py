2ebea5cd43b0f76fea10ddd2278d67df65d3eeae
bontorhumala <bontorhumala@gmail.com>
2016-09-15 16:29:56 +0200

    measuring tp fp tn fn

--- Occupancy/runner.py
+++ Occupancy/runner.py
@@ -1,7 +1,7 @@
 import subprocess;
 
-command = "python pcasvmcvconf_itercv.py ";
-tr = [0.6, 0.7, 0.8, 0.9];
+command = "python pcasvmconf_trmonth.py ";
+tr = [0.3,0.4,0.5,0.6, 0.7, 0.8, 0.9];
 sr = [1, 2, 3, 4, 5, 10, 30, 60, 300, 600, 900, 1800];
 fl = [300, 600, 900, 1800, 3600];
 h = ['r2'];
